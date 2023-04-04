8fa9d3d52ec9bbbd1f4e9886da7e04a968de0cb8
Tanay Raj Srivastava <codebytanay@gmail.com>
2023-04-04 22:18:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,78 +1,57 @@
 # Amazon Price Tracker
 
-import smtplib
-
 import requests
 from bs4 import BeautifulSoup
+from smtplib import SMTP
 
-import config
+# URl of the product
+# URL of amazon
+URL = "https://www.amazon.in/Apple-iPhone-128GB-Product-RED/dp/B0BDJVSDMY/ref=sr_1_1_sspa?crid=3ICUDBXBA3T7T&keywords=iphone+14&qid=1679603851&sprefix=iphoe+12+m%2Caps%2C4963&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&psc=1"
+# URL of Flipkart
+# URL = "https://www.flipkart.com/apple-iphone-14-starlight-128-gb/p/itm3485a56f6e676?pid=MOBGHWFHABH3G73H&lid=LSTMOBGHWFHABH3G73HVXY5AV&marketplace=FLIPKART&q=iPhone+14&store=tyy%2F4io&srno=s_1_1&otracker=search&otracker1=search&fm=organic&iid=40268296-dc19-45b8-8eeb-6c296c311626.MOBGHWFHABH3G73H.SEARCH&ppt=pp&ppn=pp&ssid=w1s1futyxguth9mo1679833517109&qH=d36118758fc7c034"
 
-# using the product suggested by the course
-PAGE_URL = "https://www.amazon.com/Instant-Pot-Duo-Evo-Plus/dp/B07W55DDFB/"
-# get a notification whenever the price falls below the following
-TARGET_PRICE = 100
-CURRENCY = "$"
-# default Mailtrap server settings
-SMTP_SERVER = "smtp.mailtrap.io"
-SMTP_PORT = "2525"
-# not really necessary, but might as well format the email properly
-SENDER_NAME = "Amazon Price Tracker"
-SENDER_ADDRESS = "pricetracker@mydomain.zyx"
-SUBJECT_TEXT = "Amazon Price Tracker Price Alert"
-# "Amazon Price Tracker Price Alert Recipient" sounded a little impersonal...
-# so using one of the names from Day 40 instead
-RECEIVER_NAME = "Anita Bath"
-RECEIVER_ADDRESS = "myemail@mydomain.zyx"
+# Set your desired price threshold for the product
+Desired_PRICE = 95000
+
+headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
+}
 
 
-def get_price():
-    """Retrieves the price from the defined page and returns it as a FLOAT."""
-    # pretend to be a proper web browser, essentially
-    headers = {
-        "Accept-Language": "en-US,en;q=0.9,ja-JP;q=0.8,ja;q=0.7",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/87.0.4280.141 Safari/537.36 "
-    }
-    response = requests.get(url=PAGE_URL, headers=headers)
-    response.raise_for_status()
-    soup = BeautifulSoup(response.text, "html.parser")
-    # using an id that's unique to the page
-    result = soup.select_one("#price_inside_buybox")
-    raw_price = result.get_text()
-    # just return the value
-    try:
-        price_num = float(raw_price.replace("$", ""))
-    except ValueError:
-        return None
-    else:
-        return price_num
+# Function to check the price
+def check_price():
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # using a class that's unique to the page
+    if 'amazon' in URL:
+        price = soup.find("span", {"class": "a-price-whole"}).get_text().replace(',', '').replace('₹', '').strip()
+    elif 'flipkart' in URL:
+        price = soup.find("div", {"class": "_30jeq3 _16Jk6d"}).get_text().replace(',', '').replace('₹', '').strip()
+
+    price = int(float(price))  # Convert the price to an integer
+    return price
 
 
-def send_email(price):
-    """Takes a price as a FLOAT and sends an email alert using the defined parameters."""
-    # format the email for the SMTP server
-    sender = f"{SENDER_NAME} <{SENDER_ADDRESS}>"
-    receiver = f"{RECEIVER_NAME} <{RECEIVER_ADDRESS}>"
-    message = f"Subject: {SUBJECT_TEXT}\nTo: {receiver}\nFrom: {sender}\n\n" \
-              f"The following product is now {CURRENCY}{price} (below the defined {CURRENCY}{TARGET_PRICE}).\n\n" \
-              f"{PAGE_URL}"
-    try:
-        with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
-            server.starttls()
-            server.login(config.SMTP_LOGIN, config.SMTP_PASS)
-            server.sendmail(sender, receiver, message)
-    except smtplib.SMTPServerDisconnected as ex:
-        print(ex)
-        print("Make sure the SMTP_LOGIN and SMTP_PASS credentials have been set correctly in config.py.")
-    else:
-        # just to have some feedback
-        print(f"A price alert was sent to {RECEIVER_ADDRESS}.")
+SMTP_SERVER = "smtp.gmail.com"
+PORT = 587
+EMAIL_ID = "codebytanay@gmail.com" # This is the Sender's Email_ID
+PASSWORD = "daixcczrbkjhdkqo"  # This is the app generated password
 
 
-# get the current price
-current_price = get_price()
+# Function to send email notification
+def notify():
+    server = SMTP(SMTP_SERVER, PORT)
+    server.starttls()
+    server.login(EMAIL_ID, PASSWORD)
 
-if current_price is None:
-    print("Could not retrieve the current price.")
-# convert to float, just to be safe
-elif current_price <= float(TARGET_PRICE):
-    send_email(current_price)
+    subject = "BUY NOW!!"
+    body = "Price has fallen. Go buy it now - " + URL
+    msg = f"Subject: {subject}\n\n{body}"
+
+    server.sendmail(EMAIL_ID, "tanayt2y@gmail.com", msg)
+    server.quit()
+
+
+# Condition to send mail
+if check_price() < Desired_PRICE:
+    notify()
